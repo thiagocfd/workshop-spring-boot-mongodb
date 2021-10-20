f7282ecbbee3bdee4ca11b36ab5ebdfaa32d096b
Thiago Dourado <thiagocfdourado@gmail.com>
2021-10-20 11:47:05 -0300

    Consulta simples com query methods

--- src/main/java/com/thiagodourado/workshopmongo/resources/PostResource.java
+++ src/main/java/com/thiagodourado/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.thiagodourado.workshopmongo.resources;
 import com.thiagodourado.workshopmongo.domain.Post;
 import com.thiagodourado.workshopmongo.domain.User;
 import com.thiagodourado.workshopmongo.dto.UserDTO;
+import com.thiagodourado.workshopmongo.resources.util.URL;
 import com.thiagodourado.workshopmongo.services.PostService;
 import com.thiagodourado.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,14 @@ public class PostResource {
         Post obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 }
