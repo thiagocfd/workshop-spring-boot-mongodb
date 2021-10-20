adedf5ae576c7f142860da3868b708a2597cc968
Thiago Dourado <thiagocfdourado@gmail.com>
2021-10-20 10:53:26 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/thiagodourado/workshopmongo/resources/UserResource.java
+++ src/main/java/com/thiagodourado/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.thiagodourado.workshopmongo.resources;
 
+import com.thiagodourado.workshopmongo.domain.Post;
 import com.thiagodourado.workshopmongo.domain.User;
 import com.thiagodourado.workshopmongo.dto.UserDTO;
 import com.thiagodourado.workshopmongo.services.UserService;
@@ -65,5 +66,13 @@ public class UserResource {
         obj = service.update(obj);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
